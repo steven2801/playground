a495e86a0fbf003d6472933e58399f8265d9439e
Steven <ssteven075@gmail.com>
2022-06-25 15:06:50 +0700

    Send api calls to spotify api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { LOGIN_URL } from "../../../lib/spotify";
 
 export default NextAuth({
 	// Configure one or more authentication providers
@@ -7,7 +8,22 @@ export default NextAuth({
 		SpotifyProvider({
 			clientId: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
+			authorization: LOGIN_URL,
 		}),
 	],
+	callbacks: {
+		async jwt({ token, account }) {
+			// Persist the OAuth access_token to the token right after signin
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token, user }) {
+			// Send properties to the client, like an access_token from a provider.
+			session.accessToken = token.accessToken;
+			return session;
+		},
+	},
 	secret: process.env.JWT_SECRET,
 });
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,59 @@
-import React from "react";
-import { Heading, Container, VStack, Button } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Heading, Container, HStack, VStack, Button, Text } from "@chakra-ui/react";
 import { signIn, useSession, signOut } from "next-auth/react";
 import { getProviders } from "next-auth/react";
+import { spotifyApi } from "../lib/spotify";
 
 export default function Home({ provider }) {
 	const { data: session } = useSession();
 	const { spotify } = provider;
 
+	useEffect(() => {
+		session && spotifyApi.setAccessToken(session.accessToken);
+	}, [session]);
+
+	const [recentTracks, setRecentTracks] = useState([]);
+
+	const fetchTracks = () => {
+		if (session.accessToken) {
+			spotifyApi.getMyRecentlyPlayedTracks().then((res) => {
+				try {
+					const {
+						body: { items },
+					} = res;
+					setRecentTracks(items);
+				} catch (err) {
+					console.log(err);
+				}
+			});
+			return true;
+		}
+		return false;
+	};
 	return (
 		<Container maxW="container.xl" h="100vh" p={4}>
-			<VStack h="full">
+			<VStack h="full" justifyContent="center" spacing={8}>
 				<Heading>Starter Template</Heading>
 				{session?.user ? (
 					<Button onClick={() => signOut()}>Sign out</Button>
 				) : (
 					<Button onClick={() => signIn(spotify.id)}>Login with spotify</Button>
 				)}
+				<Button onClick={fetchTracks} disabled={!session}>
+					Get Recently Played
+				</Button>
+				<HStack maxW="800px" flexWrap="wrap" spacing={0} gap={4}>
+					{recentTracks.map((recent, index) => {
+						const {
+							track: { name },
+						} = recent;
+						return (
+							<VStack key={index} bg="red.100" rounded="md" px={4} py={1}>
+								<Text>{name}</Text>
+							</VStack>
+						);
+					})}
+				</HStack>
 			</VStack>
 		</Container>
 	);
